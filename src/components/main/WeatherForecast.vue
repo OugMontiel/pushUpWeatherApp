969a6dd6daa6815075983cd120d3713cb0925710
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-10-21 16:57:25 -0500

    feat: :building_construction: Cambie la forma en que trabajaremos en main
    Aqui importaremos elementos para poder tratarlo por separados

---
+++ src/components/main/WeatherForecast.vue
@@ -0,0 +1,156 @@
+<template>
+    <section class="weatherForecast">
+      <div class="forecastDetails">
+        <div v-for="(inf, index) in elemenPronostico" :key="index" class="elemenPronostico">
+          <div class="circulo">
+            <span>
+              <img :src="inf.icon" :alt="inf.description" />
+            </span>
+          </div>
+          <div class="contenido">
+            <h3>{{ inf.titulo }}</h3>
+            <div class="datos">
+              <h3>{{ inf.dato }}</h3>
+              <h4>
+                <span :class="{ up: inf.varDato >= 0, down: inf.varDato < 0 }">
+                  <svg
+                    v-if="inf.varDato >= 0"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 12V4.5l4 4-1 1-3-3-3 3-1-1 4-4z"
+                    />
+                  </svg>
+                  <svg
+                    v-else
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 4v7.5l-4-4 1-1 3 3 3-3 1 1-4 4V4z"
+                    />
+                  </svg>
+                  {{ inf.varDato }}
+                </span>
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        elemenPronostico: [
+          {
+            titulo: 'Wind Speed',
+            dato: '15 km/h',
+            varDato: 2,
+            icon: '/icon/windSpeed.svg',
+            description: 'Wind icon',
+          },
+          {
+            titulo: 'Rain Chance',
+            dato: '40%',
+            varDato: -10,
+            icon: '/icon/rainChance.svg',
+            description: 'Rain icon',
+          },
+          {
+            titulo: 'Pressure',
+            dato: '1015 hPa',
+            varDato: 5,
+            icon: '/icon/pressure.svg',
+            description: 'Pressure icon',
+          },
+          {
+            titulo: 'UV Index',
+            dato: '5',
+            varDato: 1,
+            icon: '/icon/UVIndex.svg',
+            description: 'UV index icon',
+          },
+        ],
+      }
+    },
+  }
+  </script>
+  
+  <style scoped>
+  .forecastDetails {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 1em;
+    padding: 1em;
+  }
+  
+  .elemenPronostico {
+    background-color: var(--white-contraste-fondo);
+    padding: 0.5em;
+    height: 5em;
+    border-radius: 1em;
+    display: flex;
+  }
+  
+  .circulo {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .circulo span {
+    background-color: var(--white-soft);
+    width: 2em;
+    height: 2em;
+    border-radius: 50%;
+  }
+  
+  .circulo img {
+    width: 100%;
+    height: auto;
+    object-fit: contain;
+  }
+  
+  .contenido {
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    padding: 0 .5em;
+  }
+  
+  .datos {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .datos h4 {
+    gap: 0.5em;
+  }
+  
+  .datos span {
+    display: flex;
+    align-items: center;
+  }
+  
+  .up svg {
+    fill: green;
+  }
+  
+  .down svg {
+    fill: red;
+  }
+  </style>
+  
---
+++ src/components/main/WeatherHour.vue
@@ -0,0 +1,47 @@
+<!-- WeatherHour.vue -->
+<template>
+    <section class="weatherHour">
+      <div v-for="(hour, index) in weatherHours" :key="index" class="hourItem">
+        <h3>{{ hour.time }}</h3>
+        <div class="hourDetails">
+          <img :src="hour.icon" :alt="hour.description" />
+          <h4>{{ hour.temperature }}</h4>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        weatherHours: [
+          { time: 'Now', temperature: '22°C', icon: '/icon/cloud.svg', description: 'Cloudy' },
+          { time: '1h', temperature: '21°C', icon: '/icon/rain.svg', description: 'Rain' },
+          { time: '2h', temperature: '20°C', icon: '/icon/cloud.svg', description: 'Cloudy' },
+          { time: '3h', temperature: '19°C', icon: '/icon/sun.svg', description: 'Sunny' },
+        ],
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Añadir estilos para el pronóstico por horas */
+  .weatherHour {
+    display: flex;
+    justify-content: space-between;
+    padding: 1em;
+  }
+  
+  .hourItem {
+    text-align: center;
+  }
+  
+  .hourDetails {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  </style>
+  
