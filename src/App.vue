15c35386c3c2ec9b29d03eebd6cb2f02f94f5bdb
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-10-21 11:55:59 -0500

    feat: :art: Inicio de construccion del main
    pausa apra almorzar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
 import Header from './components/header.vue';
+import Main from './components/main.vue';
 </script>
 
 <template>
   <Header />
-
-  <main>relleno</main>
+  <Main />
 </template>
 
 <style scoped>
---
+++ src/components/main.vue
@@ -0,0 +1,44 @@
+<template>
+    <main>
+        <!-- Sección de elementos del pronostico pronóstico -->
+        <section class="weatherForecast">
+            <div class="forecastDetails">
+                <div v-for="(inf, index) in elemenPronostico" :key="index" class="elemenPronostico">
+                    <div class="circulo">
+                        <img :src="inf.icon" :alt="inf.description">
+                    </div>
+                    <div class="contenido">
+                        <h3>{{ inf.titulo }}</h3> <!-- Wind Speed , Rain Chance, Pressure, UV index -->
+                        <div class="datos">
+                            <h3>{{ inf.dato }}</h3>
+                            <h4>{{ inf.varDato }}</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Sección pronostico por hota -->
+        <section class="weatherHour">
+
+        </section>
+    </main>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            elemenPronostico: [
+                { titulo: 'Wind Speed', dato: '15 km/h', varDato: 'NE', icon: '/icon/wind.svg', description: 'Wind icon' },
+                { titulo: 'Rain Chance', dato: '40%', varDato: 'Moderate', icon: '/icon/rain.svg', description: 'Rain icon' },
+                { titulo: 'Pressure', dato: '1015 hPa', varDato: 'Stable', icon: '/icon/pressure.svg', description: 'Pressure icon' },
+                { titulo: 'UV Index', dato: '5', varDato: 'Moderate', icon: '/icon/uv.svg', description: 'UV index icon' },
+            ],
+
+        };
+    },
+};
+</script>
+
+<style scoped></style>
