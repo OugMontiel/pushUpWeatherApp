7c2715839a7144e021e0a5ce8d55c88c6d976d6e
Diego Alejandro Montiel Florez <88790286+OugMontiel@users.noreply.github.com>
2024-10-21 17:37:04 -0500

    feat: :bug: una pequeña modulacin mas
    para los iconos

---
+++ src/components/main/IconDisplay.vue
@@ -0,0 +1,31 @@
+<!-- IconDisplay.vue -->
+<template>
+    <span class="icon-container">
+      <img :src="icon" :alt="description" />
+    </span>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      icon: String,
+      description: String,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .icon-container {
+    width: 2em;
+    height: 2em;
+    border-radius: 50%;
+    background-color: var(--white-soft);
+  }
+  
+  .icon-container img {
+    width: 100%;
+    height: auto;
+    object-fit: contain;
+  }
+  </style>
+  
--- src/components/main/WeatherHour.vue
+++ src/components/main/WeatherHour.vue
@@ -3,83 +3,74 @@
     <section class="weatherHour">
         <div class="hourDetails">
             <div class="hourTitle">
-                <span>
-                    <img src="" alt="forecast">
-                </span>
+                <IconDisplay :icon="iconPath" description="forecast" />
                 <h3>Hourly forecast</h3>
             </div>
             <div class="hourList">
                 <div v-for="(hour, index) in weatherHours" :key="index" class="hourItem">
-                  <h3>{{ hour.time }}</h3>
-                  <div class="hourDetails">
-                    <img :src="hour.icon" :alt="hour.description" />
-                    <h4>{{ hour.temperature }}</h4>
-                  </div>
+                    <h3>{{ hour.time }}</h3>
+                    <div class="hourDetails">
+                        <img :src="hour.icon" :alt="hour.description" />
+                        <h4>{{ hour.temperature }}</h4>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        weatherHours: [
-          { time: 'Now', temperature: '22°C', icon: '/icon/cloud.svg', description: 'Cloudy' },
-          { time: '10h', temperature: '21°C', icon: '/icon/rain.svg', description: 'Rain' },
-          { time: '11h', temperature: '20°C', icon: '/icon/cloud.svg', description: 'Cloudy' },
-          { time: '12h', temperature: '19°C', icon: '/icon/sun.svg', description: 'Sunny' },
-          { time: '1h', temperature: '19°C', icon: '/icon/sun.svg', description: 'Sunny' },
-          { time: '2h', temperature: '19°C', icon: '/icon/sun.svg', description: 'Sunny' },
-        ],
-      };
+</template>
+
+<script>
+import IconDisplay from './IconDisplay.vue';
+
+export default {
+    components: {
+        IconDisplay,
     },
-  };
-  </script>
-  
-  <style scoped>
-  .hourDetails{
+    data() {
+        return {
+            weatherHours: [
+                { time: 'Now', temperature: '22°C', icon: '/icon/cloud.svg', description: 'Cloudy' },
+                { time: '10h', temperature: '21°C', icon: '/icon/rain.svg', description: 'Rain' },
+                { time: '11h', temperature: '20°C', icon: '/icon/cloud.svg', description: 'Cloudy' },
+                { time: '12h', temperature: '19°C', icon: '/icon/sun.svg', description: 'Sunny' },
+                { time: '1h', temperature: '19°C', icon: '/icon/sun.svg', description: 'Sunny' },
+                { time: '2h', temperature: '19°C', icon: '/icon/sun.svg', description: 'Sunny' },
+            ],
+        };
+    },
+};
+</script>
+
+
+<style scoped>
+.hourDetails {
     background-color: var(--white-contraste-fondo);
     padding: 0.5em;
     border-radius: 1em;
     display: flex;
     flex-direction: column;
-  }
-.hourTitle{
+}
+
+.hourTitle {
     height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
 }
 
-.hourTitle span {
-    background-color: var(--white-soft);
-    width: 2em;
-    height: 2em;
-    border-radius: 50%;
-  }
-  
-  .hourTitle img {
-    width: 100%;
-    height: auto;
-    object-fit: contain;
-  }
-
-  .hourTitle h3 {
+.hourTitle h3 {
     flex-grow: 1;
     padding: 0 .5em;
-  }
-  .hourList{
-    display: flex;
-  }
+}
 
-  .hourItem{
+.hourList {
+    display: flex;
+}
+
+.hourItem {
     display: flex;
     flex-direction: column;
     align-items: center;
     padding: 0.5em;
-  }
-  
-  </style>
-  
+}
+</style>
